d0f9c509ccfe211595464ad70e3e1e5d58177e96
Ferdinando980 <f.fernandez1@studenti.unisa.it>
2024-10-08 16:33:59 +0200

    Add files via upload
    Risoluzione di problema dipendenze Maven

--- ProgettoIs JustInTime/src/main/java/com/justInTime/demo/Player.java
+++ ProgettoIs JustInTime/src/main/java/com/justInTime/demo/Player.java
@@ -1,5 +1,5 @@
-package com.justInTime.demo;
 
+package com.justInTime.demo;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -49,4 +49,3 @@ public class Player {
             this.maxScore++;  // Aumenta il numero di partite vinte
         }
     }
-}
